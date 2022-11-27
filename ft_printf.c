7e74b67d46d1039909c61ceebbde5b0ee4e9eb3e
Kamelia Laksi <klaksi@c3r12p7.42quebec.com>
2022-11-27 17:40:12 -0500

    fixed stuff

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 16:40:15 by kamelialaks       #+#    #+#             */
-/*   Updated: 2022/11/27 16:59:33 by klaksi           ###   ########.fr       */
+/*   Updated: 2022/11/27 17:38:11 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,20 @@ int	ft_printf(const char *fmt, ...)
 
 	i = 0;
 	len = 0;
-    va_start(args, fmt);
-    while (fmt[i])
-    {
-        if (fmt[i] == '%')
-            ft_formater(args, fmt[++i] + 1);
-      else
-      {
-          len += write(1, fmt + i, 1);
-          i++;
-      }
-    }
-    va_end(args);
-    return (len);
+	va_start(args, fmt);
+	while (fmt[i])
+	{
+		if (fmt[i] == '%')
+		{
+			len += ft_formater(args, fmt[i + 1]);
+			i++;
+		}
+		else
+		{
+			len += write(1, fmt + i, 1);
+		}
+		i++;
+	}
+	va_end(args);
+	return (len);
 }

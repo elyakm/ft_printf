6ba4c0902d549b33aecc3e3b387bcfba688f5390
Kamelia Laksi <klaksi@c3r1p4.42quebec.com>
2022-11-30 10:36:34 -0500

    handling pointers

--- ft_format.c
+++ ft_format.c
@@ -6,7 +6,7 @@
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 18:08:58 by kamelialaks       #+#    #+#             */
-/*   Updated: 2022/11/30 10:28:37 by klaksi           ###   ########.fr       */
+/*   Updated: 2022/11/30 10:29:47 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,11 @@ int	ft_formater(va_list arg, const char fmt)
 		len += ft_putstr(va_arg(arg, char *));
 	else if (fmt == '%')
 		len += ft_putchar('%');
-
+	else if (fmt == 'p')
+	{
+		len += ft_print_str("0x");
+		ft_putnbr_base(va_arg(arg, unsigned long), "0123456789abcdef", &len);
+	}
+	
+	return(len);
+}

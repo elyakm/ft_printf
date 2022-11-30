13e020f30c9bf087e559c7a125414288e40a883c
Kamelia Laksi <klaksi@c3r1p4.42quebec.com>
2022-11-30 18:14:43 -0500

    merge ft_printf.c with ft_format.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,38 @@
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 16:40:15 by kamelialaks       #+#    #+#             */
-/*   Updated: 2022/11/30 10:43:33 by klaksi           ###   ########.fr       */
+/*   Updated: 2022/11/30 18:13:27 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_formater(va_list arg, const char fmt)
+{
+	int	len;
+
+	len = 0;
+	if (fmt == 'c')
+		len += ft_putchar(va_arg(arg, int));
+	else if (fmt == 's')
+		len += ft_putstr(va_arg(arg, char *));
+	else if (fmt == '%')
+		len += ft_putchar('%');
+	else if (fmt == 'p')
+	{
+		len += ft_putstr("0x");
+		ft_putnbr_base(va_arg(arg, unsigned long), "0123456789abcdef", &len);
+	}
+	else if (fmt == 'd' || fmt == 'i')
+		ft_putnbr_base(va_arg(arg, int), "0123456789", &len);
+	else if (fmt == 'u')
+		ft_putnbr_base(va_arg(arg, unsigned int), "0123456789", &len);
+	else if (fmt == 'x')
+		ft_putnbr_base(va_arg(arg, unsigned int), "0123456789abcdef", &len);
+	else if (fmt == 'X')
+		ft_putnbr_base(va_arg(arg, unsigned int), "0123456789ABCDEF", &len);
+	return (len);
+}
 
 int	ft_printf(const char *fmt, ...)
 {
